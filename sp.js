f310c289150a94bacae2ad6dd7a97b308f797c7b
doviletumaite <dovile.tumaite@yahoo.it>
2021-06-28 16:25:52 +0200

    sp5

--- sp.js
+++ sp.js
@@ -30,7 +30,7 @@ console.log(sum)
     Create a variable called "random" and assign to it a random number between 0 and 20 (it should be randomly created at each execution).
 */
 
-let random = Math.random() * 20
+let random = Math.random() * 21
   
 console.log(random)
 /* Ex.D

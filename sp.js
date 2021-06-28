2c812421f8e2d5e3eba99d0723592ec6fd4f7809
doviletumaite <dovile.tumaite@yahoo.it>
2021-06-28 15:12:52 +0200

    sp2

--- sp.js
+++ sp.js
@@ -89,7 +89,12 @@ const theBigger=whoIsBigger(43, 39);
     Write a function called "splitMe" which receives a string as a parameter and returns an array with every word in that string.
     Ex. splitMe("I love coding") => returns ["I", "Love", "Coding"]
 */
-
+function splitMe(){
+  string = "I Love Coding"
+return  string.split(" ")
+}
+const splittered=splitMe()
+console.log(splittered)
 
 /* Ex.4
     Write a function called "deleteOne" which receives a string and a boolean as parameters. If the boolean value is true it should return the string without the first letter, otherwise it should remove the last one from it.
